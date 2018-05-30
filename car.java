8f31092545d12b76e6c85c8915f739e19d09f928
Kamshory, MT <kamshory@yahoo.com>
2018-05-31 05:57:48 +0700

    Create car.java

---
+++ car.java
@@ -0,0 +1,85 @@
+import java.lang.reflect.Field;
+
+public class Car {
+	public String color = "";
+	public double weight = 0;
+	public double length = 0;
+	public double width = 0;
+	public String brand = "";
+	public double price = 0;
+	public Car()
+	{
+	}
+	public static String escapeJSON(String input)
+	{
+		String output = "";
+		if(input != null)
+		{
+			output = input.replaceAll("\"", "\\\"");
+		}
+		return output;		
+	}
+	public String toString()
+	{
+		Field[] fields = this.getClass().getDeclaredFields();
+		int i, max = fields.length;
+		String fieldName = "";
+		String fieldType = "";
+		String ret = "";
+		String value = "";
+		boolean skip = false;
+		int j = 0;
+		for(i = 0; i < max; i++)
+		{
+			fieldName = fields[i].getName().toString();
+			fieldType = fields[i].getType().toString();
+			if(i == 0)
+			{
+				ret += "{";
+			}
+			if(fieldType.equals("int") || fieldType.equals("long") || fieldType.equals("float") || fieldType.equals("double") || fieldType.equals("boolean"))
+			{
+				try 
+				{
+					value = fields[i].get(this).toString();
+				}  
+				catch (Exception e) 
+				{
+					value = "0";
+				}
+				skip = false;
+			}
+			else if(fieldType.contains("String"))
+			{
+				try 
+				{
+					value = "\""+escapeJSON((String) fields[i].get(this))+"\"";
+				} 
+				catch (Exception e) 
+				{
+					value = "\""+"\"";
+				}
+				skip = false;
+			}
+			else
+			{
+				value = "\""+"\"";
+				skip = true;
+			}
+			if(!skip)
+			{
+				if(j > 0)
+				{
+					ret += ",";
+				}
+				j++;
+				ret += "\r\n\t\""+fieldName+"\":"+value;
+			}
+			if(i == max-1)
+			{
+				ret += "\r\n}";
+			}
+		}
+		return ret;
+	}
+}
